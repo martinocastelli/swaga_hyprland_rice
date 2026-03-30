8d513eacdf591e5e4064358d51d93fa752b471f5
Martino Castelli <martinocastelli2005@gmail.com>
2026-03-30 09:43:54 +0200

    now /home line actually show the home partition size
    and fixed formatting

--- fastfetch/get_disk.sh
+++ fastfetch/get_disk.sh
@@ -11,9 +11,9 @@ s4=${s1}" / "${s2}" ("${s3}")"
 echo ${s4}
 
 # partition /home
-s1=$(df -h | grep /dev/nvme0n1 | tr -s ' ' | head -n 3 | tail -n 1 | cut -d ' ' -f 3)
-s2=$(df -h | grep /dev/nvme0n1 | tr -s ' ' | head -n 3 | tail -n 1 | cut -d ' ' -f 2)
-s3=$(df -h | grep /dev/nvme0n1 | tr -s ' ' | head -n 3 | tail -n 1 | cut -d ' ' -f 5)
+s1=$(df -h | grep /dev/nvme0n1 | tr -s ' ' | head -n 2 | tail -n 1 | cut -d ' ' -f 3)
+s2=$(df -h | grep /dev/nvme0n1 | tr -s ' ' | head -n 2 | tail -n 1 | cut -d ' ' -f 2)
+s3=$(df -h | grep /dev/nvme0n1 | tr -s ' ' | head -n 2 | tail -n 1 | cut -d ' ' -f 5)
 
 s4=${s1}" / "${s2}" ("${s3}")"
 
